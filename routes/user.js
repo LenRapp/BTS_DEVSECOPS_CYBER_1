ab01ec8e7111ba802316bdedfead093fd20120a5
LenRapp <lennyrappro@gmail.com>
2025-11-04 16:54:41 +0100

    fix: serializer.js and user.js

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const _ = require('lodash');
-const { unsafeSerialize } = require('../utils/serializer');
+const { safeSerialize } = require('../utils/serializer');
 
 router.post('/render', (req, res) => {
-  const userTemplate = req.body.template || '<%= name %>';
+  const userTemplate =  '<%= name %>'
   const compiled = _.template(userTemplate);
   const html = compiled({ name: req.body.name || 'alice' });
   res.send(html);
@@ -12,7 +12,7 @@ router.post('/render', (req, res) => {
 
 router.post('/serialize', (req, res) => {
   const payload = req.body;
-  const s = unsafeSerialize(payload);
+  const s = safeSerialize(payload);
   res.send({ serialized: s });
 });
 
